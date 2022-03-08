721023d06a7a0d16603778f843ed9433c0d73db8
magnusblaa <magnus.moeller@hotmail.com>
2022-03-08 09:14:39 +0100

    added Debtsview

---
+++ SW4FED_1/ViewModels/DebtsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW4FED_1.ViewModels
+{
+    internal class DebtsViewModel
+    {
+    }
+}
